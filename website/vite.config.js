04328aaa063669eedfc445679de1873b71e85cdc
Iurii Malchenko <i@yurique.com>
2021-02-27 21:48:31 +0200

    vite [wip]

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,25 +1,27 @@
 import {resolve} from 'path'
-import { minifyHtml, injectHtml } from 'vite-plugin-html'
+import {minifyHtml, injectHtml} from 'vite-plugin-html'
 
 const scalaVersion = '2.13'
 // const scalaVersion = '3.0.0-RC1'
-const mode = process.env.NODE_ENV
 
 // https://vitejs.dev/config/
-export default ({mode}) => ({
-  publicDir: './src/main/static/public',
-  plugins: [
-    ...(process.env.NODE_ENV === 'production' ? [minifyHtml()] : []),
-    injectHtml({
-      injectData: {
-        mode,
-        mainJS: `/target/scala-${scalaVersion}/website-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
+export default ({mode}) => {
+  const mainJS = `/target/scala-${scalaVersion}/website-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
+  return {
+    publicDir: './src/main/static/public',
+    plugins: [
+      ...(process.env.NODE_ENV === 'production' ? [minifyHtml()] : []),
+      injectHtml({
+        injectData: {
+          mode,
+          mainJS
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        'stylesheets': resolve(__dirname, './src/main/static/stylesheets')
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      'stylesheets': resolve(__dirname, './src/main/static/stylesheets')
     }
   }
-})
+}
